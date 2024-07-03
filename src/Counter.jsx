4155df14359118371a7b22796fafd8adaa569d5e
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-07-03 14:39:18 +0200

    Updated Counter component to initialize count to 0, added increment and decrement buttons, and modified global CSS styles.

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,13 +1,24 @@
 import { useState } from "react";
 
 function Counter() {
-  const [count, setCount] = useState(4);
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
 
   return (
     <>
       <div className="container">
         <h1 className="number">{count}</h1>
       </div>
+      <section className="btns-continer">
+        <button onClick={increment} className="increment">
+          +
+        </button>
+        <button onClick={decrement} className="decrement">
+          -
+        </button>
+      </section>
     </>
   );
 }
